a0d720351c13ec6725477002e2201a6446946b9c
dabeen-jung <ltea99@naver.com>
2021-06-05 20:46:46 +0900

    천단위 구분기호

---
+++ codeup-2016.py
@@ -0,0 +1,28 @@
+#2016-천단위 구분기호
+n=int(input())
+a=list(map(int,input()))
+a.reverse()
+
+stack=[]
+cnt=0
+for i in a:
+    cnt+=1
+    stack.append(i)
+    
+    
+    if(cnt%3==0 ):
+        #cnt=0
+        if(n==cnt):
+            continue
+        stack.append(',')
+        
+            
+    
+
+
+for i in range(len(stack)):
+    print(stack.pop(),end='')
+
+print()
+print()
+
