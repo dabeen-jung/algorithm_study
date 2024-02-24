912cc35a65db5fc09bbf81feb63d822568b76852
dabeen-jung <ltea99@naver.com>
2024-02-24 16:16:08 +0900

    [백트래킹] N-Queen 29668ms 198880KB

--- baekjoon/Gold/9663.py
+++ baekjoon/Gold/9663.py
@@ -1,6 +1,6 @@
 '''
 [백트래킹] N-Queen (골4)
-
+참고 https://seongonion.tistory.com/103
 * goal:
 1. 퀸끼리 '서로 공격할 수 없게' 놓는 '경우의 수'
 
@@ -17,74 +17,36 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 N = int(input())
-graph = [[0] * N for _ in range(N)]
-queen = [] #좌표 저장
+graph = [0] * N
 cnt = 0 #경우의 수
 
-#하,상,우,좌, 대각선 왼쪽, 대각선 오른족
-dx = [0, 0, 1, -1, -1, 1, 1, -1]
-dy = [1, -1, 0, 0, -1, 1, -1, 1]
 
 #2. 퀸들끼리 서로 공격하는지 여부 확인
-def attack(x,y):
-    que = deque((x,y))
-    attack_ck = False #공격
-
-    while que:
-        x,y = que.popleft()
-
-        for i in range(8):
-            #행,열,대각선 두개
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny < 0 or nx >= N or ny >= N:
-                continue
-            #if 내가 지나고 있을 때 상대가 하필 내 경로에 있음
-            if graph[nx][ny] == 2:
-                attack_ck = True
-                break
-
-            if graph[nx][ny] != 1:
-                graph[nx][ny] = 1 #방문처리
-                que.append((nx,ny))
-
-    if attack_ck: #공격
-        return False
-    else:
-        return True
+def is_attack(row):
+    for i in range(row):
+        #if 이곳에서의 행에 대한 열이 겹치는 경우거나 (공격받음)
+        if graph[row] == graph[i] \
+                or abs(graph[row] - graph[i]) == abs(row-i):  #or 대각선 관계(공격받음)인가
+            return False # 공격함
+    return True #공격받지 않음 =>cnt+=1
 
 
-
-
-
-def dfs(depth):
+# 1. 행에 퀸을 하나씩 배치
+def queen(row):
     global cnt
 
-    if depth == N: #퀸이 N개 배치됨
-        # 서로 공격할 수 없는지 여부 => ok =cnt+=1
-        if attack(queen[0][0],queen[0][1]): #공격x
-            cnt+=1
+    if row == N:
+        cnt+=1
         return
+    else:
+        for i in range(N):
+            # row = 행, i = 열
+            # ex) 0행에 1열, 1행에 3열 => [1,3,...]
+            graph[row] = i
+            # 공격이 x면? => 다음 행의 퀸 배치(함수호출)
+            if is_attack(row):
+                queen(row+1)
 
-
-    #1. 퀸을 배치,
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == 1:
-                continue
-
-            queen.append([i,j])
-            graph[i][j] = 2
-            dfs(depth + 1)
-
-            queen.pop() # 마지막 퀸 1개 제거
-            graph[i][j] = 0 #방문여부 해지
-
-
-
-dfs(0)
+queen(0)
 print(cnt)
