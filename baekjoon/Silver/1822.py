e4a158d5dd3d0707c203f6d3717a891e26795600
dabeen-jung <ltea99@naver.com>
2024-04-05 13:25:17 +0900

    [Silver4] 차집합 744ms 130196KB

---
+++ baekjoon/Silver/1822.py
@@ -0,0 +1,21 @@
+'''
+[dp] 차집합 (실4)
+
+* goal
+A에는 속하면서 집합 B에는 속하지 않는 원소
+'''
+
+a,b = map(int, input().split())
+A = list(map(int, input().split()))
+B = set(map(int, input().split()))
+cnt = 0
+result = []
+
+for v in A:
+    if v not in B:
+        cnt += 1
+        result.append(v)
+
+result.sort()
+print(cnt)
+print(' '.join(map(str,result)))
