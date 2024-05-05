e65ebad7f612e0550949d99b7ddb5e333d0071d0
dabeen-jung <ltea99@naver.com>
2024-05-05 17:58:49 +0900

    [Gold5] 무한수열 40ms, 31120KB

---
+++ baekjoon/Gold/r_1351.py
@@ -0,0 +1,28 @@
+'''
+[해쉬] 무한수열 (골5)
+⌊x⌋ -7.8 => -8이다 (
+
+메모이제이션 + top-down식
+'''
+
+import sys
+input = sys.stdin.readline
+
+#모든 경우의 수를 구하는 bottom-up (x)
+# 필요한 경우만 구하는 top-down 으로 구함
+def f(k):
+    i,j = k // P, k//Q
+
+    if k in dp:
+        return dp[k]
+    else:
+        dp[k] = f(i) + f(j)
+        return dp[k]
+
+
+N,P,Q = map(int, input().split())
+dp = {}
+dp[0] = 1
+
+
+print(f(N))
