07531b069f006f76df88345118482af71924c83e
dabeen-jung <ltea99@naver.com>
2024-05-27 20:05:06 +0900

    효정님 코드 플루이드 워셜 방법 추가

--- baekjoon/Gold/r_2660.py
+++ baekjoon/Gold/r_2660.py
@@ -66,3 +66,52 @@ for i in range(1,n+1):
 #회장 점수(깊이), 회장후보 수 , 회장후보들
 print(MIN,len(chairman))
 print(' '.join(map(str,chairman)))
+
+
+
+'''
+플루티드 워셜 방법
+- 가중치가 있는 그래프에서 최단거리를 구할 때 주로 이용
+'''
+
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+# n = int(input())
+# distance = [[sys.maxsize for  in range(n+1)] for  in range(n+1)]
+# answer = []
+
+# 그래프 입력
+# while True:
+#     s, e = map(int, input().split())
+#
+#     if s == -1 and e == -1: # 탈출
+#         break
+# #인접행렬로 입력 받아야 함 (인접 리스트 x)
+#     distance[s][e] = 1
+#     distance[e][s] = 1
+#
+#
+# 인접 행렬 초기화
+# for i in range(1, n+1):
+#     distance[i][i] = 0
+#
+#
+# 플로이드 워셜 - 최단거리로 구함(가중치 기준)
+# for k in range(1, n+1):
+#     for i in range(1, n+1):
+#         for j in range(1, n+1):
+#             if distance[i][j] > distance[i][k] + distance[k][j]:
+#                 distance[i][j] = distance[i][k] + distance[k][j]
+#
+# for i in range(1, n+1):
+#     answer.append(max(distance[i][1:]))
+#
+# 점수, 후보 수 출력
+# minval = min(answer)
+# print(minval, answer.count(minval))
+#
+# 모든 후보 출력
+# for i in range(n):
+#     if answer[i] == minval:
+#         print(i+1, end=' ')
